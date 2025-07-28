793321f450697a15fe993386b6f64496055ab61d
mxngls <Maximilian.e.hoenig@gmail.com>
2025-07-28 22:27:22 +0900

    Work towards something that in theory resembles spec adherence
    * Naive parsing of headers
    * Returning error responses when encountering unsupported methods or protocols

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     error::Error,
+    fmt,
     io::{BufRead, BufReader, Lines, Write},
     net::{TcpListener, TcpStream},
     result::Result,
@@ -11,27 +12,68 @@ use std::{
 //                     '"$http_referer" "$http_user_agent"';
 fn write_request_log() {}
 
+static HTTP_PROTOCOL: &str = "HTTP/1.1";
+
+enum HttpSupportedMethods {
+    Get,
+}
+
+impl HttpSupportedMethods {
+    fn as_str(&self) -> &'static str {
+        match self {
+            HttpSupportedMethods::Get => "GET",
+        }
+    }
+}
+
+#[derive(Debug)]
 struct HttpHeader {
     field_name: String,
     field_value: String,
 }
 
-fn parse_response_headers(lines: Lines<BufReader<&TcpStream>>) -> Result<(), Box<dyn Error>> {
-    let headers: Vec<HttpHeader> = Vec::new();
+impl fmt::Display for HttpHeader {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.field_name, self.field_value)
+    }
+}
+
+fn parse_response_headers(
+    lines: Lines<BufReader<&TcpStream>>,
+) -> Result<Vec<HttpHeader>, Box<dyn Error>> {
+    let mut headers: Vec<HttpHeader> = Vec::new();
+    let mut has_host = false;
 
     for line in lines {
-        let (field_name, field_value) = line?
+        let line = line?;
+
+        // end of headers
+        if line.is_empty() {
+            break;
+        };
+
+        let (field_name, field_value) = line
             .split_once(":")
             .ok_or_else(|| format!("Malformed header: {}", line))?;
 
+        // TODO: Currently the parsing of headers does not NOT to rfc9110.
+        // See: https://www.rfc-editor.org/rfc/rfc9110.html#name-field-order
         headers.push(HttpHeader {
-            field_name: field_name.trim(),
-            field_value: field_value.trim(),
+            field_name: field_name.trim().to_owned(),
+            field_value: field_value.trim().to_owned(),
         });
 
-        println!("{}", line);
+        // In HTTP/1.1 all headers **except** for the host header are optional
+        if !has_host && field_name == "Host" && !field_value.is_empty() {
+            has_host = true
+        };
     }
-    Ok(())
+
+    if !has_host {
+        Err("Missing Host header".into())
+    } else {
+        Ok(headers)
+    }
 }
 
 fn send_response(
@@ -42,7 +84,7 @@ fn send_response(
 ) -> Result<(), Box<dyn Error>> {
     let response_body_length = response_body.len();
     let response = [
-        format!("HTTP/1.1 {response_status} {response_status_text}"),
+        format!("{HTTP_PROTOCOL} {response_status} {response_status_text}"),
         format!("Content-Length: {response_body_length}",),
         format!("Content-Type: {}", "text/html"),
         "".to_string(),
@@ -63,7 +105,7 @@ fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error>> {
         return Err("Empty request".into());
     };
 
-    let [request_method, _request_target, _request_protocol] = request_line
+    let [request_method, _request_target, request_protocol] = request_line
         .split_whitespace()
         .collect::<Vec<&str>>()
         .try_into()
@@ -73,18 +115,40 @@ fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut response_status_text = "OK";
     let response_body = "".to_owned();
 
-    if request_method != "GET" {
+    if request_method != HttpSupportedMethods::Get.as_str() {
         response_status = "405";
         response_status_text = "Method Not Allowed";
 
         send_response(stream, response_status, response_status_text, response_body)?;
 
         return Ok(());
+    } else if request_protocol != HTTP_PROTOCOL {
+        response_status = "505";
+        response_status_text = "HTTP version not supported";
+
+        send_response(stream, response_status, response_status_text, response_body)?;
+
+        return Ok(());
     }
 
-    parse_response_headers(lines)?;
+    let headers = parse_response_headers(lines)?;
 
-    send_response(stream, response_status, response_status_text, response_body)?;
+    println!("{}", request_line);
+
+    for header in &headers {
+        println!("{}: {}", header.field_name, header.field_value);
+    }
+
+    send_response(
+        stream,
+        response_status,
+        response_status_text,
+        headers
+            .iter()
+            .map(|h| h.to_string())
+            .collect::<Vec<_>>()
+            .join("<br>"),
+    )?;
 
     Ok(())
 }
