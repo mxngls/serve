6ec376586362641596b555a93672d8393208d28d
mxngls <Maximilian.e.hoenig@gmail.com>
2025-08-04 16:58:58 +0900

    Add rudimentary extension based Mime type support

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,87 @@ pub struct HttpFileServer {
     logger: Logger,
 }
 
+/// taken from <https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types/Common_types/>
+/// NOTE: This list is __obviously__ not exhaustive yet but should for now suffice for our needs for
+/// now
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Mime {
+    GZip,
+    Json,
+    JsonLd,
+    Binary,
+    Xml,
+    Zip,
+    Gif,
+    Jpeg,
+    Png,
+    Svg,
+    ICalendar,
+    Css,
+    Csv,
+    Html,
+    JavaScriptModule,
+    JavaScript,
+    Markdown,
+    PlainText,
+}
+
+impl Mime {
+    /// Returns possible file extensions for this MIME type
+    const fn as_str(&self) -> &'static str {
+        match self {
+            Self::GZip => "application/gzip",
+            Self::Json => "application/json",
+            Self::JsonLd => "application/ld+json",
+            Self::Binary => "application/octet-stream",
+            Self::Xml => "application/xml",
+            Self::Zip => "application/zip",
+            Self::Gif => "image/gif",
+            Self::Jpeg => "image/jpeg",
+            Self::Png => "image/png",
+            Self::Svg => "image/svg+xml",
+            Self::ICalendar => "text/calendar",
+            Self::Css => "text/css",
+            Self::Csv => "text/csv",
+            Self::Html => "text/html",
+            Self::JavaScriptModule | Self::JavaScript => "text/javascript",
+            Self::Markdown => "text/markdown",
+            Self::PlainText => "text/plain",
+        }
+    }
+}
+
+impl FromStr for Mime {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "gz" => Ok(Self::GZip),
+            "json" => Ok(Self::Json),
+            "jsonld" => Ok(Self::JsonLd),
+            "bin" => Ok(Self::Binary),
+            "xml" => Ok(Self::Xml),
+            "zip" => Ok(Self::Zip),
+            "gif" => Ok(Self::Gif),
+            "jpeg" | "jpg" => Ok(Self::Jpeg),
+            "png" => Ok(Self::Png),
+            "svg" => Ok(Self::Svg),
+            "ics" => Ok(Self::ICalendar),
+            "css" => Ok(Self::Css),
+            "csv" => Ok(Self::Csv),
+            // technically .htm would be valid here as well but in
+            // the context of where and how we intent to use the
+            // server we will omit this here on purpose
+            "html" => Ok(Self::Html),
+            "mjs" => Ok(Self::JavaScriptModule),
+            "js" => Ok(Self::JavaScript),
+            "md" => Ok(Self::Markdown),
+            "txt" => Ok(Self::PlainText),
+            _ => Err("Unkown file extension".into()),
+        }
+    }
+}
+
 impl HttpFileServer {
     pub fn new(host: &str, port: u16, logger: Logger) -> Result<Self, Box<dyn std::error::Error>> {
         let addr = format!("{host}:{port}");
@@ -32,41 +113,54 @@ impl HttpFileServer {
         })
     }
 
-    fn handle_connection(&self, stream: TcpStream) -> Result<(), Box<dyn Error>> {
-        let request = self.build_request(&stream)?;
+    fn handle_connection(&self, stream: &TcpStream) -> Result<(), Box<dyn Error>> {
+        let request = HttpRequest::from_stream(stream)?;
 
-        let source_dir = env::current_dir()?;
-        let project_dir = source_dir.parent().unwrap();
+        let project_dir = env::current_dir()?;
         let file_path = project_dir.join(request.path.trim_start_matches('/'));
+
+        println!("{}", file_path.display());
 
         if request.method != HttpMethod::Get {
             return Self::send_response(
-                &stream,
+                stream,
                 &HttpResponse::new(HttpStatus::MethodNotAllowed, None, None),
             );
         }
 
         if request.version != HttpVersion::HTTP1_1 {
             return Self::send_response(
-                &stream,
+                stream,
                 &HttpResponse::new(HttpStatus::HttpVersionNotSupported, None, None),
             );
         }
 
         let response_body = match file_path.try_exists() {
-            Ok(true) => fs::read_to_string(file_path)?,
+            Ok(true) => fs::read_to_string(&file_path)?,
             _ => {
                 return Self::send_response(
-                    &stream,
+                    stream,
                     &HttpResponse::new(HttpStatus::NotFound, None, None),
                 );
             }
         };
+
         let mut headers = HttpHeaders::new();
-        headers.insert("Content-Type".to_string(), "text/plain".to_string());
+
+        // handle the case where ther might be no extension or invalid UTF-8
+        let mime_type_str = file_path
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .and_then(|ext_str| ext_str.parse().ok())
+            .map_or(Mime::Binary, |mime| mime)
+            .as_str()
+            .to_string();
+
+        headers.insert("Content-Type".to_string(), mime_type_str);
+
         let response = HttpResponse::new(HttpStatus::Ok, Some(headers), Some(response_body));
 
-        Self::send_response(&stream, &response)?;
+        Self::send_response(stream, &response)?;
         self.logger
             .write_request_log(&request, &response, &stream.peer_addr()?.to_string())?;
 
@@ -75,74 +169,16 @@ impl HttpFileServer {
 
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         for stream in self.listener.incoming() {
-            self.handle_connection(stream?)?;
+            self.handle_connection(&stream?)?;
         }
         Ok(())
-    }
-
-    fn parse_request_headers(
-        lines: &mut Lines<BufReader<&TcpStream>>,
-    ) -> Result<HttpHeaders, Box<dyn Error>> {
-        let mut headers = HttpHeaders::new();
-        let mut has_host = false;
-
-        for line in lines {
-            let line = line?;
-
-            // end of headers
-            if line.is_empty() {
-                break;
-            }
-
-            let (field_name, field_value) = line
-                .split_once(':')
-                .map(|(f1, f2)| (f1.trim(), f2.trim()))
-                .ok_or_else(|| format!("Malformed header: {line}"))?;
-
-            // TODO: Currently the parsing of headers does not NOT conform to rfc9110.
-            // See: https://www.rfc-editor.org/rfc/rfc9110.html#name-field-order
-            headers.insert(field_name.trim().to_owned(), field_value.trim().to_owned());
-
-            // In HTTP/1.1 all headers **except** for the host header are optional
-            if !has_host && field_name == "Host" && !field_value.is_empty() {
-                has_host = true;
-            }
-        }
-
-        if has_host {
-            Ok(headers)
-        } else {
-            Err("Missing Host header".into())
-        }
-    }
-
-    fn build_request(&self, stream: &TcpStream) -> Result<HttpRequest, Box<dyn Error>> {
-        let mut lines = BufReader::new(stream).lines();
-
-        let Some(request_line) = lines.next().transpose()? else {
-            return Err("Empty request".into());
-        };
-
-        let [method_str, target, version_str] = request_line
-            .split_whitespace()
-            .collect::<Vec<&str>>()
-            .try_into()
-            .map_err(|_| "Invalid request line format")?;
-        let headers = Self::parse_request_headers(&mut lines)?;
-
-        let method = method_str.parse()?;
-        let version = version_str.parse()?;
-
-        let request = HttpRequest::new(method, target.to_string(), version, Some(headers), None);
-
-        Ok(request)
     }
 
     fn send_response(
         mut stream: &TcpStream,
         response: &HttpResponse,
     ) -> Result<(), Box<dyn Error>> {
-        write!(stream, "{}", response)?;
+        write!(stream, "{response}")?;
 
         stream.flush()?;
 
@@ -215,7 +251,6 @@ impl Logger {
         let referer = headers.get("Referer").map_or("-", |h| h);
         let user_agent = headers.get("User-Agent").map_or("-", |h| h);
 
-        // default logging used by Nginx (nginx)
         let log_line = LogFormat::Combined.format(
             remote_addr,
             &remote_user,
@@ -347,6 +382,64 @@ impl HttpRequest {
             body,
         }
     }
+
+    pub fn from_stream(stream: &TcpStream) -> Result<Self, Box<dyn Error>> {
+        let mut lines = BufReader::new(stream).lines();
+
+        let Some(request_line) = lines.next().transpose()? else {
+            return Err("Empty request".into());
+        };
+
+        let [method_str, target, version_str] = request_line
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .try_into()
+            .map_err(|_| "Invalid request line format")?;
+        let headers = Self::parse_request_headers(&mut lines)?;
+
+        let method = method_str.parse()?;
+        let version = version_str.parse()?;
+
+        let request = Self::new(method, target.to_string(), version, Some(headers), None);
+
+        Ok(request)
+    }
+
+    fn parse_request_headers(
+        lines: &mut Lines<BufReader<&TcpStream>>,
+    ) -> Result<HttpHeaders, Box<dyn Error>> {
+        let mut headers = HttpHeaders::new();
+        let mut has_host = false;
+
+        for line in lines {
+            let line = line?;
+
+            // end of headers
+            if line.is_empty() {
+                break;
+            }
+
+            let (field_name, field_value) = line
+                .split_once(':')
+                .map(|(f1, f2)| (f1.trim(), f2.trim()))
+                .ok_or_else(|| format!("Malformed header: {line}"))?;
+
+            // TODO: Currently the parsing of headers does not NOT conform to rfc9110.
+            // See: https://www.rfc-editor.org/rfc/rfc9110.html#name-field-order
+            headers.insert(field_name.trim().to_owned(), field_value.trim().to_owned());
+
+            // In HTTP/1.1 all headers **except** for the host header are optional
+            if !has_host && field_name == "Host" && !field_value.is_empty() {
+                has_host = true;
+            }
+        }
+
+        if has_host {
+            Ok(headers)
+        } else {
+            Err("Missing Host header".into())
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -358,11 +451,25 @@ pub struct HttpResponse {
 }
 
 impl HttpResponse {
-    const fn new(status: HttpStatus, headers: Option<HttpHeaders>, body: Option<String>) -> Self {
+    fn new(status: HttpStatus, headers: Option<HttpHeaders>, body: Option<String>) -> Self {
+        let mut headers = headers.unwrap_or_default();
+
+        // add default headers
+        if let Some(ref body) = body {
+            if !headers.contains_key("Content-Length") {
+                let body_len = body.len();
+                headers.insert("Content-Length".to_string(), body_len.to_string());
+            }
+
+            if !headers.contains_key("Content-Type") {
+                headers.insert("Content-Type".to_string(), "text/plain".to_string());
+            }
+        }
+
         Self {
             version: HttpVersion::HTTP1_1,
             status,
-            headers,
+            headers: Some(headers),
             body,
         }
     }
@@ -374,11 +481,6 @@ impl fmt::Display for HttpResponse {
 
         // status line
         writeln!(f, "{} {} {}", self.version, self.status.code(), self.status)?;
-
-        if !body.is_empty() {
-            writeln!(f, "Content-Length: {}", body.len())?;
-            writeln!(f, "Content-Type: text/plain")?;
-        }
 
         // headers
         if let Some(headers) = &self.headers {
